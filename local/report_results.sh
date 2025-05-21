8d5636ee2ffc2146feb1b012de0547f5e8e9f6e3
czguo <1220590388@qq.com>
2025-05-22 02:20:49 +0800

    delete local

--- local/report_results.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python parse_results.py \
-    /gscratch/h2lab/micdun/mimir/results_new/mia_unified_mia_v5_linear_tds_recency_hyp_ref/*/* \
-    --output mia_unified_mia_v5_linear_tds_recency_hyp_ref_results.json
